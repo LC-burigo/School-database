8f328f6f4a0a9520eecbf4d19d1d335b477efff2
LC-burigo <burigo-jm@hotmail.com>
2020-11-10 12:09:11 -0300

    fix some functions

--- Students.py
+++ Students.py
@@ -17,11 +17,18 @@ class Tables:
         self.Cursor = Connection.cursor()
 
     def Create_Table_students(self, name):
-        get = "CREATE TABLE {}(id INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(100));".format(name)
+        get = "CREATE TABLE {}(\
+                                id INT AUTO_INCREMENT PRIMARY KEY,\
+                                first_name VARCHAR(100\
+                                ));".format(name)
         self.Cursor.execute(get)
 
     def Create_Table_papers(self, name):
-        get = "CREATE TABLE {}(title VARCHAR(100), grade INT, student_id INT, FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE);".format(name)
+        get = "CREATE TABLE {}(\
+                                title VARCHAR(100),\
+                                grade INT, student_id INT,\
+                                FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE\
+                                );".format(name)
         self.Cursor.execute(get)
 
     def Insert_Many_Into_students(self, content):
@@ -43,21 +50,38 @@ class Tables:
         print(self.Cursor.rowcount, "ok")
 
     def students_table(self):
-        get = "SELECT first_name, IFNULL(title, 'MISSING'), IFNULL(grade, 0) FROM students LEFT JOIN papers ON students.id = papers.student_id;"
+        get = "SELECT\
+                    first_name,\
+                    IFNULL(title, 'MISSING'),\
+                    IFNULL(grade, 0)\
+                        FROM students LEFT JOIN papers ON students.id = papers.student_id;"
         self.Cursor.execute(get)
         self.Connection.commit()
         for x in self.Cursor:
             print(x)
 
     def Grade_Average(self):
-        get = "SELECT first_name, IFNULL(AVG(grade), 0) AS average FROM students LEFT JOIN papers ON students.id = papers.student_id GROUP BY students.id ORDER BY average DESC;"
+        get = "SELECT\
+                    first_name,\
+                    IFNULL(AVG(grade), 0) AS average\
+                        FROM students LEFT JOIN papers ON students.id = papers.student_id\
+                            GROUP BY students.id ORDER BY average DESC;"
         self.Cursor.execute(get)
         self.Connection.commit()
         for x in self.Cursor:
             print(x)
 
     def Status(self):
-        get = " SELECT first_name, Ifnull(Avg(grade), 0) AS average, CASE WHEN Avg(grade) IS NULL THEN 'FAILING' WHEN Avg(grade) >= 75 THEN 'PASSING' ELSE 'FAILING' end AS passing_status FROM students LEFT JOIN papers ON students.id = papers.student_id GROUP  BY students.id ORDER  BY average DESC;"
+        get = " SELECT\
+                    first_name,\
+                    Ifnull(Avg(grade), 0) AS average,\
+                    CASE\
+                      WHEN Avg(grade) IS NULL THEN 'FAILING'\
+                      WHEN Avg(grade) >= 75 THEN 'PASSING'\
+                      ELSE 'FAILING'\
+                    end AS passing_status\
+                        FROM students LEFT JOIN papers ON students.id = papers.student_id\
+                            GROUP  BY students.id ORDER  BY average DESC;"
         self.Cursor.execute(get)
         self.Connection.commit()
         for x in self.Cursor:
